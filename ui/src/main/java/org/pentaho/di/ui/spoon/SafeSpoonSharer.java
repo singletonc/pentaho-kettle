580312c98b379edf0d80bd9bf26c2f0efce17f45
Charles Singleton <csingleton@pentaho.com>
2018-01-11 11:13:29 -0500

    more, small changes

--- ui/src/main/java/org/pentaho/di/ui/spoon/SafeSpoonSharer.java
+++ ui/src/main/java/org/pentaho/di/ui/spoon/SafeSpoonSharer.java
@@ -3,6 +3,13 @@ package org.pentaho.di.ui.spoon;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
+/**
+ * This class is used to share the staticSpoon instance with SWTBot.
+ * SWTBot needs access to Spoon's shell in its constructor.
+ *
+ * @author csingleton
+ */
+
 public class SafeSpoonSharer {
     private static volatile Spoon spoon;
     private static final CyclicBarrier spoonSetBarrier = new CyclicBarrier(2);
